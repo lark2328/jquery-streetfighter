88291bb94fbb83f2c4c4bd156cb43bae77676ef6
lark2328 <shinra_girl23@yahoo.com>
2014-04-10 19:27:36 -0400

    java added

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function() {
+
+$(".ryu").mouseenter(function() {
+	$(".ryu-still").hide();
+	$(".ryu-ready").show();
+});
+
+$(".ryu").mouseleave(function() {
+	$(".ryu-still").show();
+	$(".ryu-ready").hide();
+
+});
+
+
+});
