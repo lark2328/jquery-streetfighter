6fbf732d98d0289c4ded5f58594d28b7c20b4bac
lark2328 <shinra_girl23@yahoo.com>
2014-04-12 13:14:39 -0400

    updated jquery code

--- js/app.js
+++ js/app.js
@@ -2,15 +2,40 @@
 $(document).ready(function() {
 
 $(".ryu").mouseenter(function() {
-	$(".ryu-still").hide();
-	$(".ryu-ready").show();
+	$('.ryu-still').hide();
+	$('.ryu-ready').show();
+})
+
+	.mouseleave(function() {
+	$('.ryu-ready').hide();
+	$('.ryu-still').show();
+
+})
+
+	.mousedown(function() {
+	playHadouken();
+	$('.ryu-ready').hide();
+	$('.ryu-throwing').show();
+	$('.hadouken').finish().show()
+		.animate(
+		{'left': '300px'},
+		500,
+		function() {
+			$(this).hide();
+			$(this).css('left', '-212px');
+});
+})
+
+	.mouseup(function() {
+	$('.ryu-ready').show();
+	$('.ryu-throwing').hide();
+
 });
 
-$(".ryu").mouseleave(function() {
-	$(".ryu-still").show();
-	$(".ryu-ready").hide();
-
 });
 
-
-});
+function playHadouken () {
+	$('#hadouken-sound')[0].volume= 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
